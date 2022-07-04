409612673671370609d58914d6b99c7d18f0097e
koceanie <kocean@xanny.us>
2022-07-04 17:52:50 +0900

    runserver

--- whataboutme/whataboutme/settings.py
+++ whataboutme/whataboutme/settings.py
@@ -24,14 +24,13 @@ ALLOWED_HOSTS = [ '*' ]
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     #'django.contrib.admin',
     #'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
