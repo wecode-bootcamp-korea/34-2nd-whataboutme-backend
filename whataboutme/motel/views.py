5d2c82f00144f2ecf4c19022eb4b68071983b7c6
koceanie <kocean@xanny.us>
2022-07-11 19:12:11 +0900

    api maden, problem with themes_list data

--- whataboutme/motel/views.py
+++ whataboutme/motel/views.py
@@ -14,45 +14,39 @@ from motel.models import Motel, Room, Reservation, Theme, RoomTheme
 class MotelListQuery(View) : 
     def get(self, request):
         try : 
-            my_location = request.GET.get('my_location')
+            themes = []
+            my_latitude = request.GET.get('my_latitude')
+            my_longitude = request.GET.get('my_longitude')
             distance = request.GET.get('distance')
-            themes = request.GET.getlist('themes[]')
-            checkinout = request.GET.get('checkinout')
-            price = request.GET.get('price')
+            themes_list = request.GET.getlist("themes[]", None)
+            checkin = request.GET.get('checkin')
+            checkout = request.GET.get('checkout')
+            pricemin = request.GET.get('pricemin')
+            pricemax = request.GET.get('pricemax')
 
             q = Q()
+            
+            print(themes_list)
 
-            if my_location : 
-                my_latitude, my_longitude = list(map(str, my_location))
-            if distance :
-                q &= Q(distance = distance)
-            if themes :
-                q &= Q(themes = themes[])
-            if checkinout : 
-                checkin, checkout = list(map(str, checkinout))
-            if price:
-                pricemin, pricemax = list(map(str, price))
+        #     motel_query = Room.objects.filter(q) \
+        #         .prefetch_related('motel', 'reservation_room', 'room_theme' )
 
-            print (q)
-
-            motel_query = Room.objects.filter(q) \
-                .prefetch_related('motel', 'reservation_room', 'room_theme' )
-
-            # motel = 룸과 연결된 모텔
-            # reservation_room = 예약 
-            # room_theme = 룸테마
+        #     # motel = 룸과 연결된 모텔
+        #     # reservation_room = 예약 
+        #     # room_theme = 룸테마
 
 
 
-            results = [
-                {
-                    'id' : room.motel.id,
-                    'name' : room.motel.name,
-                    'address' : room.motel.address,
-                    'price' : room.discount_price.first()
+        #     results = [
+        #         {
+        #             'id' : room.motel.id,
+        #             'name' : room.motel.name,
+        #             'address' : room.motel.address,
+        #             'price' : room.discount_price.first()
 
-                } for room in motel_query
-            ]
+        #         } for room in motel_query
+        #     ]
+            return JsonResponse({"message": 'SUCCESS', "theme": themes_list, "checkin":checkin, "checkout":checkout}, status=200)
         except JSONDecodeError:
             return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status=400)
         except KeyError:
